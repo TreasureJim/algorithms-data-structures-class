9446cb245c484f7dfb3e3af8eed27011c24addc7
Liam Boreback <boreback.work@gmail.com>
2023-11-08 21:10:33 +0100

    Work on setup funcs

--- seminar1/src/main.rs
+++ seminar1/src/main.rs
@@ -1,7 +1,24 @@
 mod task1;
 
 fn main() {
-    println!("Hello, world!");
 }
 
+mod test_helpers {
+    pub fn generate_random_list(n: u32) -> (Vec<i32>, Vec<i32>) {
+        let mut v = vec![0; n as usize];
+        for _ in 0..n {
+            v.push(rand::random::<i32>());
+        }
 
+        let mut vs = v.clone();
+        vs.sort();
+        (v, vs)
+    }
+
+    const TEST_FILE_PATH: &str = "./random_numbers.txt";
+
+    pub fn read_test_file() -> Vec<i32> {
+        let v: Vec<&str> = std::fs::read_to_string(TEST_FILE_PATH).unwrap().split("\r\n").collect();
+        v.iter().map(|x| )
+    }
+}
--- seminar1/src/task1.rs
+++ seminar1/src/task1.rs
@@ -1,3 +1,8 @@
-fn quick_sort(arr: Vec) {
+fn quick_sort<T>(arr: Vec<impl PartialOrd>) {}
 
+mod test {
+    use crate::test_helpers::generate_random_list;
+
+    #[test]
+    fn test_file() {}
 }
