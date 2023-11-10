863db90ca7cd5a91fdc87fdb67344c797715cb38
Liam Boreback <boreback.work@gmail.com>
2023-11-10 02:59:27 +0100

    Progress quick sort

--- seminar1/src/task1.rs
+++ seminar1/src/task1.rs
@@ -8,34 +8,28 @@ pub fn quick_sort_recursive<T: PartialOrd + Copy>(arr: &mut [T]) {
     }
 
     let len = arr.len();
-    let mut low = 0;
-    let mut high = arr.len() - 1;
 
-    let pivot_index = len / 2;
-    let pivot = arr[pivot_index];
+    let pivot_index = 0;
+    let pivot = arr[0];
 
-    while low < high {
-        while arr[low] < pivot {
-            low += 1;
-        }
-        while arr[high] > pivot {
-            high -= 1;
-        }
+    let mut lp = pivot_index + 1;
 
-        if arr[low] >= pivot && arr[high] <= pivot {
-        // swap
-        let temp = arr[low];
-        arr[low] = arr[high];
-        arr[high] = temp;
-        low += 1;
-        high -= 1;
+    for i in (pivot_index + 1)..len {
+        if arr[i] <= pivot {
+            // swap i and lp
+            let temp = arr[i];
+            arr[i] = arr[lp];
+            arr[lp] = temp;
+            lp += 1;
         }
     }
+    //swap pivot index, lp - 1
+    let temp = arr[lp - 1];
+    arr[lp - 1] = arr[pivot_index];
+    arr[pivot_index] = temp;
 
-    assert!(low == high);
-
-    quick_sort_recursive(&mut arr[0..high]);
-    quick_sort_recursive(&mut arr[high..len])
+    quick_sort_recursive(&mut arr[0..lp]);
+    quick_sort_recursive(&mut arr[lp..len])
 }
 
 // fn partition<T: PartialOrd>(mut arr: &Vec<T>, low: usize, high: usize) {
@@ -48,10 +42,18 @@ mod test {
     use crate::test_helpers::generate_random_list;
 
     #[test]
-    fn test_file() {
+    fn test1() {
+        let mut unsort = [5, 8, 10, 1, 2];
+        let mut sorted = unsort.clone();
+        sorted.sort();
+        quick_sort_recursive(&mut unsort);
+        assert_eq!(unsort, sorted);
+    }
+
+    #[test]
+    fn test_random() {
         let mut r = generate_random_list(10, 0, 100);
         quick_sort_recursive(&mut r.0);
-        dbg!(&r.0, &r.1);
         assert_eq!(r.0, r.1);
     }
 }
