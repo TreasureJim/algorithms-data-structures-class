76362008ae96bc92e956d52dcb88704affe93aed
TreasureJim <boreback.work@gmail.com>
2023-11-14 19:54:58 +0100

    Work on quicksort

--- seminar1/src/task1.rs
+++ seminar1/src/task1.rs
@@ -24,14 +24,6 @@ pub fn quick_sort_simple_recursive<T: PartialOrd + Copy>(mut arr: &mut [T]) {
         } else {
             n_pivot += 1;
         }
-        while arr[rp] >= pivot {
-            rp -= 1;
-        }
-
-        // (LP and RP have stopped now) LP is point at large element and RP is pointing at small element
-        if lp < rp {
-            swap(arr, lp, rp);
-        }
     }
 
     quick_sort_simple_recursive(&mut smaller);
@@ -45,50 +37,56 @@ pub fn quick_sort_simple_recursive<T: PartialOrd + Copy>(mut arr: &mut [T]) {
     arr[index..].clone_from_slice(&bigger);
 }
 
-pub fn quick_sort_recursive<T: PartialOrd + Copy>(arr: &mut [T]) {
+pub fn insertion_sort_iter<T: Clone>(arr: &mut [T]) {
+    if arr.len() <= 1 {
+        return;
+    }
+
     // TODO: for small arrays use different sorting algorithm
+    for (xi, x) in arr.iter().enumerate() {
+        for (yi, y) in arr.iter().enumerate() {}
+    }
+}
+
+pub fn quick_sort_recursive<T: PartialOrd + Copy>(arr: &mut [T]) {
     if arr.len() <= 1 {
         return;
     }
 
     let len = arr.len();
 
-    let pivot_index = 0;
+    // choosing pivot
+    let pivot_index = len - 1;
     let pivot = arr[pivot_index];
 
     // partition
-    // 	1. swap pivot element with last element
-    // 2. LP = 0, RP = len - 2 (one before the last element)
-    // 3. while LP <= RP && arr[LP] < pivot: LP++
-    // 4. while RP >= pivot: RP --
-    // 5. if LP < RP: swap LP and RP elements
-    // 6. stop when LP and RP pass each other
-    // every element before LP is now small
-    // for n < 10 || 20 use insertion sort
 
-    swap(arr, pivot_index, len - 1);
+    // 	1. swap pivot element with last element
+    arr.swap(pivot_index, len - 1);
+    // 2. LP = 0, RP = len - 2 (one before the last element)
     let mut lp = 0;
     let mut rp = len - 2;
-    let mut change = true;
 
-    while (lp <= rp) {
-        while arr[lp] < pivot && lp <= rp {
+    while (lp < rp) {
+        // (LP and RP have stopped now) LP is point at large element and RP is pointing at small element
+        if arr[lp] > pivot && arr[rp] < pivot {
+            arr.swap(lp, rp);
             lp += 1;
-        }
-        while arr[rp] >= pivot {
             rp -= 1;
         }
-
-        // (LP and RP have stopped now) LP is point at large element and RP is pointing at small element
-        if lp < rp {
-            swap(arr, lp, rp);
+        // otherwise move pointers
+        else if arr[lp] <= pivot {
+            lp += 1;
+        } else if arr[rp] >= pivot {
+            rp -= 1;
         }
     }
 
-    swap(arr, len - 1, lp);
+    arr.swap(len - 1, lp);
 
-    quick_sort_recursive(&mut arr[0..lp]);
-    quick_sort_recursive(&mut arr[lp..len])
+    // recursive
+    quick_sort_recursive(&mut arr[..lp]);
+    quick_sort_recursive(&mut arr[lp + 1..]);
 }
 
 fn swap<T>(arr: &mut [T], i1: usize, i2: usize) {
@@ -110,21 +108,23 @@ fn swap<T>(arr: &mut [T], i1: usize, i2: usize) {
 #[cfg(test)]
 mod test {
     use super::quick_sort_simple_recursive;
-    use crate::test_helpers::generate_random_list;
+    use crate::{task1::quick_sort_recursive, test_helpers::generate_random_list};
 
     #[test]
     fn test1() {
-        let mut unsort = [5, 8, 10, 1, 2];
+        let mut unsort = [1, 1, 1, 22];
+        dbg!(&unsort);
         let mut sorted = unsort.clone();
         sorted.sort();
-        quick_sort_simple_recursive(&mut unsort);
+        quick_sort_recursive(&mut unsort);
         assert_eq!(unsort, sorted);
     }
 
     #[test]
     fn test_random() {
-        let mut r = generate_random_list(10_000_000, 0, 100);
-        quick_sort_simple_recursive(&mut r.0);
+        let mut r = generate_random_list(10, 0, 100);
+        dbg!(&r.0);
+        quick_sort_recursive(&mut r.0);
         assert_eq!(r.0, r.1);
     }
 }
